860e0fe55cfe832cbc7c1185a3b867018c539de8
Jerzy Brzezinski <jerzy.brzezinski@salesagility.com>
2016-08-30 09:18:03 +0100

    Fixed #2049 7.7.2 - Calendar Activities are off by 1 day

--- config_override.php
+++ config_override.php
@@ -1,3 +1,10 @@
 <?php
 /***CONFIGURATOR***/
+$sugar_config['lead_conv_activity_opt'] = 'copy';
+$sugar_config['default_module_favicon'] = false;
+$sugar_config['dashlet_auto_refresh_min'] = '30';
+$sugar_config['enable_action_menu'] = true;
+$sugar_config['hide_subpanels'] = false;
+$sugar_config['stack_trace_errors'] = false;
+$sugar_config['developerMode'] = false;
 /***CONFIGURATOR***/
